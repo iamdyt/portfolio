908bdda1c7ec089631ef46ac3c281fe404947bca
iamdyt <olanlokunlanre@gmail.com>
2020-06-24 12:56:14 -0700

    redesign

--- styles/dist/js/alert.js
+++
@@ -1,9 +0,0 @@
-  // ALERTS
-  ButterCake.plugin('alert', function () {
-    $('.alert .alert-close').on('click', function () {
-      var $this = $(this);
-      $this.parent('.dismissable').fadeOut('fast', function () {
-        $this.parent('.dismissable').remove();
-      })
-    });
-  }, true);
--- styles/dist/js/chips.js
+++
@@ -1,9 +0,0 @@
-  // CHIPS
-  ButterCake.plugin('chip', function () {
-    $('.chip .btn-clear-chip').on('click', function () {
-      var $this = $(this);
-      $this.closest('.chip').fadeOut('fast', function () {
-        $this.closest('.chip').remove();
-      })
-    })
-  }, true);
